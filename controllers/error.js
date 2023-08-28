72fccbe0650e94bfac2f4e9fab4f9c22bbaec6b1
sathwk <sathwik.kotla@zemosolabs.com>
2023-08-28 18:07:07 +0530

    working project with sequelize

---
+++ controllers/error.js
@@ -0,0 +1,7 @@
+const path = require('path');
+
+const rootDir = require("../util/path");
+
+exports.get404Page = (req,res,next) => {
+    res.status(404).render('404', {pageTitle: 'Page Not Found', path:'/404'});
+}
