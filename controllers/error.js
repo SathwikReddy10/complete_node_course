e74d6237be2631adef27c984a82da724563d6954
sathwk <sathwik.kotla@zemosolabs.com>
2023-08-29 14:18:45 +0530

    working app with nosql mongo db

--- controllers/error.js
+++ controllers/error.js
@@ -1,7 +1,3 @@
-const path = require('path');
-
-const rootDir = require("../util/path");
-
-exports.get404Page = (req,res,next) => {
-    res.status(404).render('404', {pageTitle: 'Page Not Found', path:'/404'});
-}
+exports.get404 = (req, res, next) => {
+    res.status(404).render('404', { pageTitle: 'Page Not Found', path: '/404' });
+  };
